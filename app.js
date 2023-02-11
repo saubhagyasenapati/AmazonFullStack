b2d3ec0b34c6a33cbc8c791140ff346658cb09f5
Saubhagya <sikan2601@gmail.com>
2023-02-11 18:48:44 +0530

    v6

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-dotenv.config({ path: "backend/config/config.env" });
-
+dotenv.config({ path: "config/config.env" });
+const path=require("path")
 //Handling Uncaught exception
 process.on("uncaughtException", (err) => {
   console.log(`Error:${err.message}`);
@@ -34,10 +34,10 @@ app.use("/api/v1", orderRoute);
 app.use("/api/v1", paymentRoute);
 
 
-// app.use(express.static(path.join(___dirname,"../frontend/build")))
-// app.get("*",(req,res)=>{
-//   res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
-// })
+app.use(express.static(path.join(__dirname,"../frontendn/build")))
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"../frontendn/build/index.html"));
+})
 //MiddleWare for error
 app.use(errorMiddleware);
 app.listen(process.env.PORT, () => {
